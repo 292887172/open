a6d557d266a16d2e9d4647e5f5888d2d8994766e
zhanlingjie <734422495@qq.copm>
2017-09-27 19:33:46 +0800

    更新目录:static/ng/product/oven demo.html stove.html smoke.html oven.html 更新内容: 修改oven stove smoke样式 更改demo布局

--- static/demo/js/my_poppicker.js
+++ static/demo/js/my_poppicker.js
@@ -27,8 +27,8 @@
 		return [].slice.call($.__create_dom_div__.childNodes);
 	};
 
-	var panelBuffer = '<div class="mui-poppicker" id="picture" style="width:58%;margin-left: 55px;">\
-		<h4 style="padding-left:15px;padding-top: 15px">设置烘烤温度：<span id="block-range-tem" style="font-size: 10px;color:#f35019">200℃\
+	var panelBuffer = '<div class="mui-poppicker" id="picture" style="width:82%;margin-left: 55px;border-radius: 25px;">\
+		<h4 style="padding-left:15px;padding-top: 15px;font-size: 16px;">设置烘烤温度：<span id="block-range-tem" style="font-size: 17px;color:#f35019">200℃\
 		</span></h4>\
 		<div class="mui-input-row mui-input-range">\
 		<div id="range-progressbar" class="mui-progressbar">\
