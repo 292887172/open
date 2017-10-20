b814345f1b21766f56373f9358c8ca41fa28eeab
zhanlingjie <734422495@qq.copm>
2017-10-20 11:23:01 +0800

    change demo.html

--- common/util.py
+++ common/util.py
@@ -178,6 +178,57 @@ def send_test_device_status(did, status):
             pass
         return obj_res
 
+    # 油烟机发送数据
+    # default = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # ID | 模式 | 温度 | 时间 | 工作状态 | 操作 | 风扇 | 电灯 | -->
+    #  tmp_status = {'did': userID, 'model': devices.oven_type, 'temp': Oven_temp, 'time': nowtime, 'status': status, 'wind': wind, 'light': light};
+
+    if did =='test15267183467-smoke':
+        if status:
+            oven_model = status.get('model')
+            oven_temp = status.get('temp')
+            oven_time =status.get('time')
+            oven_status =status.get('status')
+            oven_wind =status.get('wind')
+            oven_light =status.get('light')
+            default[0] = oven_model
+            default[1] =oven_temp
+            default[2]=oven_time
+            default[3]=oven_status
+            default[4]=oven_wind
+            default[5]=oven_light
+        deal_test_device_status(did,default,'set')
+        try:
+            conn = stomp.Connection10(host_and_ports=[(STOMP_ADDR, STOMP_PORT)])
+            conn.start()
+            conn.connect()
+            conn2 = stomp.Connection10(host_and_ports=[(STOMP_ADDR2, STOMP_PORT)])
+            conn2.start()
+            conn2.connect()
+            obj = {"id": did, "type": int(2500), "value": default}
+            print('发送内容', obj)
+            # 往支持stomp协议的消息队列中添加数据
+            conn.send(body=json.dumps({"msg": obj}), destination="/" + did)
+            conn.disconnect()
+            conn2.send(body=json.dumps({"msg": obj}), destination="/" + did)
+            conn2.disconnect()
+            obj_res = "OK"
+        except Exception as e:
+            print('往消息队列中发送消息出错:' + str(e))
+            # 请求格式错误
+            obj_res = "ERR"
+            pass
+        return obj_res
+
+
+
+
+
+
+
+
+
+
 
 
 
