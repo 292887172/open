34b7befe5afc2234ae4c2cf3a4937813a148ee2f
gmy <1305373037@qq.com>
2017-11-01 10:08:05 +0800

    微信扫码推送

--- conf/apiconf.py
+++ conf/apiconf.py
@@ -8,4 +8,4 @@ wx_userinfo = "https://api.weixin.qq.com/sns/userinfo?access_token={0}&openid={1
 
 wx_ticket = "http://wechat.53iq.com/partner/ddb4c038579a11e59e8800a0d1eb6068/jsapi_ticket?token=hi"
 INSIDE_MESSAGE_PUSH="https://api.53iq.com/1/message/push?access_token={0}"
-DOWNLOAD_ZIP = "http://oven.53iq.com/static/file?key={0}.zip"
+DOWNLOAD_ZIP = "http://oven.53iq.com/static/file/{0}.zip"
