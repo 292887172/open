6a5df84bd958d891d5558d1f1c23d008fabb01ba
gmy <1305373037@qq.com>
2017-11-24 12:44:41 +0800

    产品功能提交bug修改

--- static/ng/product/edit/controller.js
+++ static/ng/product/edit/controller.js
@@ -16,10 +16,14 @@ angular.module('Product.edit', ['ngRoute'])
          * @constructor
          */
         $scope.Save = function () {
-
         	var state = checkID();
-			if (state !='correct' || !checkName() || !checkLength()){
+			if (state !='correct' || !checkName()){
 				return;
+			}
+			if($.trim($('#mxsLength').val())==''){
+				$('#checkLength').html("长度不能为空!!");
+				$('#checkLength').css("display","block");
+				return ;
 			}
 				var min=0;
 				var max=0;
