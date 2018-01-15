1136cb433dd85c6fdf73ca01c8c0e114366fb6cd
Jayzhanscar <734422495@qq.com>
2018-01-15 19:09:46 +0800

    添加app:api 添加接口：get_ui_conf  保存自定义ui配置数据          pull_ui_conf  获取ui配置数据

---
+++ api/views.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import HttpResponse
+from common.mysql_helper import query_data, get_ui_base_conf
+import json
+
+# Create your views here.
+
+
+@csrf_exempt
+def pull_ui_conf(request):
+    if request.method == 'POST':
+        device_key = request.POST.get('key')
+        if device_key:
+            a = query_data(device_key)
+            if a:
+                new_functions = []
+
+                function_list = json.loads(a['ebf_pc_conf'])['functions']
+                print(json.loads(a['ebf_pc_conf'])['name'])
+                for i in function_list:
+                    new_functions.append({'name': i['name'], "id": i['id'], 'title': i['title']})
+
+                json.loads(a['ebf_pc_conf'])['functions'] = new_functions
+
+                new_list = {'name':  json.loads(a['ebf_pc_conf'])['name'], 'key': json.loads(a['ebf_pc_conf'])['key'],
+                            "model":  json.loads(a['ebf_pc_conf'])['model'], "functions": new_functions}
+                back_data = {"data": new_list, "code": 0}
+                return HttpResponse(json.dumps(back_data))
+            else:
+                return HttpResponse('not fonud conf')
+        else:
+            return HttpResponse('not found key')
+
+
+@csrf_exempt
+def get_ui_conf(request):
+    if request.method == 'POST':
+        device_id = request.POST.get('key')
+        device_conf = request.POST.get('ui_conf')
+        print(device_id, device_conf)
+        if device_id:
+            if device_conf:
+                back_data = get_ui_base_conf(device_id, device_conf)
+                if back_data == 'ok':
+                    data = {'code': 1, 'msg': 'ok'}
+                    return HttpResponse(json.dumps(data), content_type="application/json")
+                else:
+                    data = {'code': 0, 'msg': 'save failed'}
+                    return HttpResponse(json.dumps(data), content_type="application/json")
+            else:
+                data = {'code': -1, 'msg': 'no conf'}
+                return HttpResponse(json.dumps(data), content_type="application/json")
+        else:
+            data = {'code': -1, 'msg': 'no key'}
+            return HttpResponse(json.dumps(data), content_type="application/json")
