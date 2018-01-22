c8a4ad98072a0d30e66cdff101f8e464446ace07
Jayzhanscar <734422495@qq.com>
2018-01-22 13:53:27 +0800

    添加app:api 添加接口：get_ui_conf  保存自定义ui配置数据          pull_ui_conf  获取ui配置数据

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.views.decorators.csrf import csrf_exempt
 from django.http.response import HttpResponse
-from common.mysql_helper import query_data, get_ui_base_conf
+from common.mysql_helper import query_data, get_ui_base_conf, query_ui_conf
 import json
 
 # Create your views here.
@@ -18,7 +18,7 @@ def pull_ui_conf(request):
                 new_functions = []
 
                 function_list = json.loads(a['ebf_pc_conf'])['functions']
-                print(json.loads(a['ebf_pc_conf'])['name'])
+                # print(json.loads(a['ebf_page_conf'])['name'])
                 for i in function_list:
                     new_functions.append({'name': i['name'], "id": i['id'], 'title': i['title']})
 
@@ -39,15 +39,15 @@ def get_ui_conf(request):
     if request.method == 'POST':
         device_id = request.POST.get('key')
         device_conf = request.POST.get('ui_conf')
-        print(device_id, device_conf)
         if device_id:
             if device_conf:
                 back_data = get_ui_base_conf(device_id, device_conf)
+                print(back_data)
                 if back_data == 'ok':
-                    data = {'code': 1, 'msg': 'ok'}
+                    data = {'code': 0, 'msg': 'ok'}
                     return HttpResponse(json.dumps(data), content_type="application/json")
                 else:
-                    data = {'code': 0, 'msg': 'save failed'}
+                    data = {'code': 1, 'msg': 'save failed'}
                     return HttpResponse(json.dumps(data), content_type="application/json")
             else:
                 data = {'code': -1, 'msg': 'no conf'}
@@ -55,3 +55,13 @@ def get_ui_conf(request):
         else:
             data = {'code': -1, 'msg': 'no key'}
             return HttpResponse(json.dumps(data), content_type="application/json")
+
+@csrf_exempt
+def diy_ui_conf(request):
+    if request.method == 'GET':
+        device_key = request.GET.get('key')
+        if device_key:
+            back_data = query_ui_conf(device_key)
+            print(back_data)
+            if back_data != 'error':
+                return HttpResponse({'code':1})
