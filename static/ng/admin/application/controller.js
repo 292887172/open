c9ba23039f67804a0667b64ef85dd34542d84a0c
cherry <rendy@56iq.com>
2018-02-05 20:03:04 +0800

    后台管理增加微信账号显示，和全部应用和以审核应用的区分显示

--- static/ng/admin/application/controller.js
+++ static/ng/admin/application/controller.js
@@ -15,6 +15,9 @@ angular.module('Admin.application', ['ngRoute'])
         }).when('/application/check', {
             templateUrl: '/static/ng/admin/application/check.html',
             controller : 'ApplicationCheckCtrl'
+        }).when('/application/published', {
+            templateUrl: '/static/ng/admin/application/published.html',
+            controller : 'ApplicationPublishedCtrl'
         });
     }])
 
@@ -88,6 +91,34 @@ angular.module('Admin.application', ['ngRoute'])
                 if (data.code == 10000) {
                     mmg.removeRow($(event.target).attr("data-row"));
                     $("#passModal").modal("hide");
+                }
+            }).error(function (error) {
+                alert(error);
+            })
+        };
+    }])
+
+    /**
+ * 已审核通过应用列表控制器
+ */
+    .controller('ApplicationPublishedCtrl', ['$scope', "$http", function ($scope, $http) {
+        $scope.checkFormData = {
+            "csrfmiddlewaretoken": $scope.csrf_token,
+            "app_id"             : "",
+            "remark"             : ""
+        };
+        $scope.failProduct = function (event) {
+            $scope.checkFormData.app_id = $(event.target).attr("data-id");
+            $scope.checkFormData.action = "fail_product";
+            $http({
+                method : "POST",
+                url    : location.href,
+                data   : $.param($scope.checkFormData),
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function (data) {
+                if (data.code == 10000) {
+                    mmg.removeRow($(event.target).attr("data-row"));
+                    $("#failModal").modal("hide");
                 }
             }).error(function (error) {
                 alert(error);
