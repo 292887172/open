08cf0c4508f701203fbe493c5ab4df94406ae0f3
liuwu <292887172@qq.com>
2018-07-05 17:31:58 +0800

    快速了解页面

--- static/ng/product/protocol/controller.js
+++ static/ng/product/protocol/controller.js
@@ -589,6 +589,20 @@ angular.module('Product.protocol', ['ngRoute'])
                 headers: {'Content-Type': 'application/x-www-form-urlencoded'}
             }).success(function (response1) {
                // console.log('拿回的数据',response1)
+
+                jQuery.extend({
+                        alertWindow:function(e,n){var e=e,r;n===undefined?r="#00a8b7":r=n;
+                        if($("body").find(".alertWindow1").length===0){
+                            var i='<div class="alertWindow1" style="width: 100%;height: 100%; background:rgba(0,0,0,0.5);position: fixed; left:0px; top: 0px; z-index: 9999;"><div  style="width: 360px; height: 170px;background: #FFF;margin: 300px auto;border: 2px solid #CFCFCF;">'+'<div  style="width: inherit;height: 20px;">'+'<div class="alertWindowCloseButton1" style="float: right; width: 10px; height: 30px;margin-right:5px;font-family:\'microsoft yahei\';color:'+r+';cursor: pointer;"></div>'+"</div>"+'<div id="successImg" class="alertWindowTitle" style="margin-top:10px;width:100px;text-align:center;font-family:\'Verdana, Geneva, Arial, Helvetica, sans-serif\';font-size: 18px;font-weight: normal;color: '+r+';">'+"</div>"+'<div class="alertWindowContent" style="width:360px;height: 40px;text-align:center;font-size: 18px;color: #7F7F7F;margin-top:10px;">'+e+"</div>"+"</div>"+"</div>";
+                            $("body").append(i);
+                            var s=$(".alertWindow1");
+                            //2秒后自动关闭窗口
+                            setTimeout(function(){s.hide()},1000);
+                        }
+                        else {$(".alertWindowContent").text(e),$(".alertWindow1").show(),setTimeout(function(){$(".alertWindow1").hide()},1000);}
+                        }
+                        })
+                jQuery.alertWindow("保存成功");
                 $scope.response = response1;
 
                 $scope.response.protocol_type = response1.protocol_type;
