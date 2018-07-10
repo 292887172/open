5bffab507240691514016d9b5297804d42198566
liuwu <292887172@qq.com>
2018-07-11 07:08:18 +0800

    蒸烤一体

--- base/util.py
+++ base/util.py
@@ -1398,6 +1398,407 @@ def gen_app_default_conf(val):
                 }],
          'mxsLength': '8', 'id': '18', 'Stream_ID': 'ERROR', 'toSwitch': '0', 'isFunction': '1'},
     ]
+    oven_zkx = [
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '开关', 'corpMark': '', 'widget': 'button', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [{'desc': '关', 'trigger':[],'data': '0'}, {'desc': '开','trigger':[], 'data': '1'}], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '1', 'Stream_ID': 'POWER', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '系统保留', 'corpMark': '', 'widget': 'button', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [
+                {
+                    "desc":"无",
+                    "data":"0"
+                },
+                {
+                    "desc":"可按",
+                    "data":"1"
+                },
+                {
+                    "desc":"转考",
+                    "trigger":[
+
+                    ],
+                    "data":"2"
+                },
+                {
+                    "desc":"保留",
+                    "trigger":[
+
+                    ],
+                    "data":"4"
+                }
+            ], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '2', 'Stream_ID': 'FAN', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '童锁', 'corpMark': '', 'widget': 'button', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [
+                {
+                    "desc":"关",
+                    "data":"0"
+                },
+                {
+                    "desc":"开",
+                    "data":"1"
+                }
+            ], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '3', 'Stream_ID': 'BODY_LOCK', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '照明', 'corpMark': '', 'widget': 'button', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [
+                {
+                    "desc":"关",
+                    "data":"0"
+                },
+                {
+                    "desc":"开",
+                    "data":"1"
+                }
+            ], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '4', 'Stream_ID': 'LAMP', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '工作状态', 'corpMark': '', 'widget': 'button', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [
+                {
+                    "desc":"无",
+                    "trigger":[
+
+                    ],
+                    "data":"0"
+                },
+                {
+                    "desc":"工作",
+                    "trigger":[
+
+                    ],
+                    "data":"1"
+                },
+                {
+                    "desc":"停止",
+                    "trigger":[
+
+                    ],
+                    "data":"2"
+                },
+                {
+                    "desc":"暂停",
+                    "trigger":[
+
+                    ],
+                    "data":"3"
+                }
+            ], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '5', 'Stream_ID': 'STATE', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '模式', 'corpMark': '', 'widget': 'button', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [
+                {
+                    "desc":"解冻",
+                    "data":"1"
+                },
+                {
+                    "desc":"发酵",
+                    "data":"2"
+                },
+                {
+                    "desc":"杀菌",
+                    "data":"3"
+                },
+                {
+                    "desc":"清洗",
+                    "data":"4"
+                },
+                {
+                    "desc":"高温",
+                    "data":"5"
+                },
+                {
+                    "desc":"保温",
+                    "data":"6"
+                },
+                {
+                    "desc":"蔬菜",
+                    "trigger":[
+
+                    ],
+                    "data":"256"
+                },
+                {
+                    "desc":"水果",
+                    "trigger":[
+
+                    ],
+                    "data":"512"
+                },
+                {
+                    "desc":"肉类",
+                    "trigger":[
+
+                    ],
+                    "data":"768"
+                },
+                {
+                    "desc":"鱼类",
+                    "trigger":[
+
+                    ],
+                    "data":"1024"
+                },
+                {
+                    "desc":"蛋类",
+                    "trigger":[
+
+                    ],
+                    "data":"1280"
+                },
+                {
+                    "desc":"米饭",
+                    "trigger":[
+
+                    ],
+                    "data":"1536"
+                },
+                {
+                    "desc":"面食",
+                    "trigger":[
+
+                    ],
+                    "data":"1792"
+                },
+                {
+                    "desc":"快热",
+                    "trigger":[
+
+                    ],
+                    "data":"4096"
+                },
+                {
+                    "desc":"风焙烤",
+                    "trigger":[
+
+                    ],
+                    "data":"4097"
+                },
+                {
+                    "desc":"焙烤",
+                    "trigger":[
+
+                    ],
+                    "data":"4098"
+                },
+                {
+                    "desc":"底加热",
+                    "trigger":[
+
+                    ],
+                    "data":"4099"
+                },
+                {
+                    "desc":"风扇烤",
+                    "trigger":[
+
+                    ],
+                    "data":"4100"
+                },
+                {
+                    "desc":"烧烤",
+                    "trigger":[
+
+                    ],
+                    "data":"4101"
+                },
+                {
+                    "desc":"强烧烤",
+                    "trigger":[
+
+                    ],
+                    "data":"4102"
+                },
+                {
+                    "desc":"增强烧烤",
+                    "trigger":[
+
+                    ],
+                    "data":"4103"
+                },
+                {
+                    "desc":"发酵",
+                    "trigger":[
+
+                    ],
+                    "data":"4104"
+                },
+                {
+                    "desc":"蛋糕",
+                    "trigger":[
+
+                    ],
+                    "data":"4352"
+                },
+                {
+                    "desc":"家禽",
+                    "trigger":[
+
+                    ],
+                    "data":"4608"
+                },
+                {
+                    "desc":"面包",
+                    "trigger":[
+
+                    ],
+                    "data":"4864"
+                },
+                {
+                    "desc":"烤肉",
+                    "trigger":[
+
+                    ],
+                    "data":"5120"
+                },
+                {
+                    "desc":"烤鱼",
+                    "trigger":[
+
+                    ],
+                    "data":"5376"
+                },
+                {
+                    "desc":"披萨",
+                    "trigger":[
+
+                    ],
+                    "data":"5632"
+                },
+                {
+                    "desc":"饼干",
+                    "trigger":[
+
+                    ],
+                    "data":"5888"
+                }
+
+            ], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '6', 'Stream_ID': 'MODEL', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '设定温度', 'corpMark': '', 'widget': 'input', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '7', 'Stream_ID': 'SET_TEMPE', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '建议温度下限', 'corpMark': '', 'widget': 'input', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '8', 'Stream_ID': 'Suggest_Temp_Down', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '建议温度上限', 'corpMark': '', 'widget': 'input', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '9', 'Stream_ID': 'Suggest_Temp_Up', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '当前温度', 'corpMark': '', 'widget': 'input', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '10', 'Stream_ID': 'CUR_TEMPE', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '设定工作时间', 'corpMark': '', 'widget': 'input', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '11', 'Stream_ID': 'SET_WORK_TIME', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '建议工作时间下限', 'corpMark': '', 'widget': 'input', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '12', 'Stream_ID': 'Suggest_Time_Down', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '建议工作时间上限', 'corpMark': '', 'widget': 'input', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '13', 'Stream_ID': 'Suggest_Time_Up', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '剩余工作时间', 'corpMark': '', 'widget': 'button', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '14', 'Stream_ID': 'REMAIN_WORK_TIME', 'toSwitch': '0', 'isFunction': '1'},
+        {'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '状态反馈', 'corpMark': '', 'widget': 'button', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [
+                {
+                    "desc":"门关",
+                    "trigger":[
+
+                    ],
+                    "data":"0"
+                },
+                {
+                    "desc":"门开",
+                    "trigger":[
+
+                    ],
+                    "data":"1"
+                },
+                {
+                    "desc":"水位",
+                    "trigger":[
+
+                    ],
+                    "data":"2"
+                },
+                {
+                    "desc":"水箱",
+                    "trigger":[
+
+                    ],
+                    "data":"4"
+                },
+                {
+                    "desc":"童锁开",
+                    "trigger":[
+
+                    ],
+                    "data":"8"
+                }
+            ], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '15', 'Stream_ID': 'System_Status', 'toSwitch': '0', 'isFunction': '1'},{'isControl': 1, 'state': 1, 'isDisplay': 1, 'isCloudMenu': 0, 'isShow': 0, 'time': '2017-08-01 09:20:19',
+         'name': '故障', 'corpMark': '', 'widget': 'button', 'min': 0, 'max': 255,
+         'mxsNum': '2', 'mxs': [
+                {
+                    "desc":"无",
+                    "trigger":[
+
+                    ],
+                    "data":"0"
+                },
+                {
+                    "desc":"箱短路",
+                    "trigger":[
+
+                    ],
+                    "data":"1"
+                },
+                {
+                    "desc":"箱开路",
+                    "trigger":[
+
+                    ],
+                    "data":"2"
+                },
+                {
+                    "desc":"热短路",
+                    "trigger":[
+
+                    ],
+                    "data":"4"
+                },
+                {
+                    "desc":"热开路",
+                    "trigger":[
+
+                    ],
+                    "data":"8"
+                },
+                {
+                    "desc":"缺水",
+                    "trigger":[
+
+                    ],
+                    "data":"16"
+                }
+            ], 'corpName': '', 'paramType': 4,
+         'mxsLength': '8', 'id': '16', 'Stream_ID': 'ERROR', 'toSwitch': '0', 'isFunction': '1'},
+    ]
     other = [
         {'isControl': 1, 'state': 1, 'isDisplay':1,'isCloudMenu':0,'isShow':0, 'time': '2017-08-01 09:20:19', 'name': '开关', 'corpMark': '', 'widget': 'button', 'min': 0, 'max': 255,
          'mxsNum': '2', 'mxs': [{'desc': '关', 'data': '0'}, {'desc': '开', 'data': '1'}], 'corpName': '', 'paramType': 4,
@@ -1422,6 +1823,9 @@ def gen_app_default_conf(val):
     elif val == 20:
         # 蒸箱
         return steam
+    elif val == 21:
+        # 蒸烤箱
+        return oven_zkx
     elif val == 27:
         # wifi小烤箱
         return small_oven
