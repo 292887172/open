2b7c874503ef5e8637e254631cf76c1d3217dafc
By2048 <1247079575@qq.com>
2018-08-13 16:12:52 +0800

    代码稳定性维护，测试示例整理，文档整理

--- common/config_helper.py
+++ common/config_helper.py
@@ -2,79 +2,114 @@
 import django
 import json
 import pprint
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 django.setup()
 
 from model.center.app import App
 from model.center.protocol import Protocol
 
+"""
+默认使用Django的ORM进行数据查询，单独运行此py文件时，在pycharm中配置
+PYTHONUNBUFFERED=1;DJANGO_SETTINGS_MODULE=open.settings
+"""
 
-def get_device_function(key):
+
+def get_device_function(key: str) -> list:
+    """获取用户定义的帧协议
+    :param key: 用户的key
+    :return: 成功 -》 用户定义的帧协议  ，  失败 -》 false
+    """
     try:
         app = App.objects.get(app_appid__contains=key)
     except Exception as e:
-        pass
-    configs = json.loads(app.device_conf)
-    device_function = []
-    for config in configs:
-        item = {}
-        item['length'] = int(config['mxsLength'])
-        item['name'] = config['Stream_ID']
-        item['title'] = config['name']
-
-        if config['Stream_ID'] == 'Fan3':
-            item['controls'] = {'Main': 113}
-
-        device_function.append(item)
-    return device_function
-
-
-name_map = {
-    'frame_head': 'head',
-    'frame_type': 'type',
-    'frame_length': 'length',
-    'data_domain': 'data'
-}
+        logging.error('查询数据失败 key=%s' % key)
+        logging.error(str(e))
+        return False
+    try:
+        configs = json.loads(app.device_conf)
+        device_function = []
+        for config in configs:
+            item = {}
+            item['length'] = int(config['mxsLength'])
+            item['name'] = config['Stream_ID']
+            item['title'] = config['name']
+            # TODO 测试时默认绑定一个UI ID ，后续需要删除
+            if config['Stream_ID'] == 'Fan3':
+                item['controls'] = {'Main': 113}
+            device_function.append(item)
+        return device_function
+    except Exception as e:
+        logging.error(str(e))
+        return False
 
 
-def get_data_length(key):
+def get_data_length(key: str) -> int:
+    """
+    根据用户定义的功能列表，计算自定义帧协议中 data_domain 的数据长度
+    :param key: 用户的Key
+    :return: 成功 数据域长度  失败 false
+    """
     try:
         app = App.objects.get(app_appid__contains=key)
+        configs = json.loads(app.device_conf)
+        data_len = 0
+        for config in configs:
+            data_len += int(config['mxsLength'])
+        return int(data_len / 8)
     except Exception as e:
-        pass
-    configs = json.loads(app.device_conf)
-    data_len = 0
-    for config in configs:
-        data_len += int(config['mxsLength'])
-    return int(data_len / 8)
+        logging.error(str(e))
+        return False
 
 
-def get_device_protocol_config(key):
+def get_device_protocol_config(key: str):
+    """
+    :param key: 用户的key
+    :return: 成功 》 用户的自定义协议配置  ，  失败 》 False
     """
 
-    :param key:
-    :return:  list  [0]
-    """
-    protocols = Protocol.objects.filter(protocol_device_key=key)
+    # 命名转换。数据库与帧代码中保持一致
+    name_map = {
+        'frame_head': 'head',
+        'frame_type': 'type',
+        'frame_length': 'length',
+        'data_domain': 'data'
+    }
+    try:
+        protocols = Protocol.objects.filter(protocol_device_key=key)
+    except Exception as e:
+        logging.error('key=' + key + ' ' + str(e))
 
-    items = []
+    if not protocols:
+        logging.error('protocols ' + str(protocols))
+        return False
+
+    configs = []
     for protocol in protocols:
         protocol_type = protocol.protocol_factory_type
         protocol_content = protocol.protocol_factory_content
-        config = json.loads(protocol_content)
+        try:
+            config = json.loads(protocol_content)
+        except Exception as e:
+            logging.error('转换失败 ' + str(protocol_content))
+            logging.error(str(e))
 
-        if protocol_type == 0:
+        try:
             item = {}
             item['endian_type'] = int(config['endian_type'])
             item['length_offset'] = 'None'
             item['check_type'] = config['checkout_algorithm']
             item['check_data_start'] = int(config['start_check_number'])
             item['check_data_end'] = int(config['end_check_number'])
+
             structs = []
             for frame_content in config['frame_content']:
                 _item = {}
                 _item['name'] = frame_content['name']
-                if _item['name'] in name_map.keys(): _item['name'] = name_map[_item['name']]
+                if _item['name'] in name_map.keys():
+                    _item['name'] = name_map[_item['name']]
                 try:
                     if frame_content['name'] == 'data_domain':
                         _item['length'] = get_data_length(key)
@@ -94,63 +129,31 @@ def get_device_protocol_config(key):
                 if not _item['value']: del _item['value']
                 structs.append(_item)
             item['structs'] = structs
-            _sum = 0
-            for struct in structs:
-                try:
-                    _sum += struct['length']
-                except KeyError:
-                    pass
-            item['length'] = _sum
+            item['length'] = sum([struct['length'] for struct in structs])
 
-            items.insert(0, item)
-        else:
-            item = {}
-            item['endian_type'] = int(config['endian_type'])
-            item['length_offset'] = 'None'
-            item['check_type'] = config['checkout_algorithm']
-            item['check_data_start'] = int(config['start_check_number'])
-            item['check_data_end'] = int(config['end_check_number'])
-            structs = []
-            for frame_content in config['frame_content']:
-                _item = {}
-                _item['name'] = frame_content['name']
-                if _item['name'] in name_map.keys(): _item['name'] = name_map[_item['name']]
-                try:
-                    if frame_content['name'] == 'data_domain':
-                        _item['length'] = get_data_length(key)
-                    else:
-                        _item['length'] = int(frame_content['length'])
-                except KeyError:
-                    pass
-                _item['value'] = []
-                if len(frame_content['code']) > 0:
-                    for code in frame_content['code']:
-                        if len(code['value']) == 4:
-                            _item['value'].append(int(code['value'][0:2], 16))
-                            _item['value'].append(int(code['value'][2:4], 16))
-                        else:
-                            _item['value'].append(int(code['value'], 16))
-                if not _item['value']: del _item['value']
-                structs.append(_item)
-            item['structs'] = structs
-            _sum = 0
-            for struct in structs:
-                try:
-                    _sum += struct['length']
-                except KeyError:
-                    pass
-            item['length'] = _sum
+            if protocol_type == 0:
+                configs.insert(0, item)
+            else:
+                configs.append(item)
+        except Exception as e:
+            logging.error(str(e))
+    if configs:
+        return configs
+    else:
+        return False
 
-            items.append(item)
-    return items
+
+def test_get_device_function():
+    device_function = get_device_function(key='q8qG3tq7')
+    pprint.pprint(device_function)
+
+
+def test_get_device_protocol_config():
+    device_protocol_config = get_device_protocol_config(key='q8qG3tq7')
+    pprint.pprint(device_protocol_config, width=200, indent=4)
 
 
 if __name__ == '__main__':
-    device_function = get_device_function(key='vUN5MyX2')
-    pprint.pprint(device_function)
+    test_get_device_function()
 
-    print('\n')
-
-    device_protocol_config = get_device_protocol_config(key='vUN5MyX2')
-    pprint.pprint(device_protocol_config[0], width=200)
-    pprint.pprint(device_protocol_config[1], width=200)
+    test_get_device_protocol_config()
--- conf/mysqlconf.py
+++ conf/mysqlconf.py
@@ -6,13 +6,6 @@ MYSQL_PWD = "53iq.com"
 MYSQL_DB = "ebdb_open"
 
 
-# MYSQL_HOST = "122.144.167.73"
-# MYSQL_PORT = 3306
-# MYSQL_USER = "root"
-# MYSQL_PWD = "53iq.com"
-# MYSQL_DB = "ebdb_open"
-
-
 # 设备管理系统mysql数据库配置
 MYSQL_HOST_SYS = "192.168.0.62"
 MYSQL_PORT_SYS = 3306
